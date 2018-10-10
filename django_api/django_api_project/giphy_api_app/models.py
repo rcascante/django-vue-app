08c2a795a7a4de51b71ce6b0599ad24893e93cb0
Margita Cascante <margitacascante@APSLs-Mac-mini.local>
2018-10-10 16:23:06 +0200

    [UPDATE] requirements.txt

---
+++ django_api/django_api_project/giphy_api_app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class GiphyData(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.CharField(max_length=2083)
+    
+    def __str__(self):
+        return self.name
